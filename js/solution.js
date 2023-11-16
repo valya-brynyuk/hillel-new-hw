b63cec23a8cc110ee52bfb5b84cdd0dbae2894a4
Bryniuk Valia <valyanychyporuk@gmail.com>
2023-11-17 00:01:19 +0200

    - completed

---
+++ js/solution.js
@@ -0,0 +1,16 @@
+'use strict';
+
+(() => {
+  document.addEventListener('DOMContentLoaded', () => {
+    const items = document.querySelectorAll('ul > li');
+    const texts = [];
+
+    for (const item of Array.from(items)) {
+      console.log(item);
+
+      texts.push(item.innerText);
+    }
+
+    console.log(texts);
+  });
+})();
