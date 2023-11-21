496889a00c6fc7b6236ff9065ac45886e14f3f2d
Bryniuk Valia <valyanychyporuk@gmail.com>
2023-11-21 16:09:50 +0200

    - completed

--- js/solution.js
+++ js/solution.js
@@ -1,25 +1,31 @@
 'use strict';
 
 (() => {
-  document.addEventListener('DOMContentLoaded', () => {
-    const list = document.querySelector('ul');
-
-    const attrs = [];
-
-    const attrNames = [];
-    for (const attr of Array.from(list.attributes)) {
-      attrs.push(attr.value);
-      attrNames.push(attr.name);
+  const generateList = (numArr) => {
+    if (!Array.isArray(numArr)) {
+      throw new Error('Only arrays with numbers are allowed');
     }
 
-    console.log(attrs);
-    console.log(attrNames);
+    const list = document.createElement('ul');
+    for (const item of numArr) {
+      const li = document.createElement('li');
+      if (Array.isArray(item)) {
+        li.appendChild(generateList(item));
+      } else if (typeof item !== 'number') {
+        throw new Error('Only numbers supported in array');
+      } else {
+        li.innerText = item;
+      }
 
+      list.appendChild(li);
+    }
+
+    return list;
+  };
+
+  document.addEventListener('DOMContentLoaded', () => {
     requestAnimationFrame(() => {
-      list.lastElementChild.innerText = `Hello, my name is Valia`;
+      document.body.appendChild(generateList([1, 2, 3, [4, 5, 6, [7, 8], 9], 0]));
     });
-
-    list.firstElementChild.dataset.myName = 'Valia';
-    delete list.dataset.dogTail;
   })
 })();
