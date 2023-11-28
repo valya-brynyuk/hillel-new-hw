2cf948b0b0d568407dc6ae8d119dbf14a81806b0
Bryniuk Valia <valyanychyporuk@gmail.com>
2023-11-29 01:56:54 +0200

    - complete

--- js/solution.js
+++ js/solution.js
@@ -1,24 +1,96 @@
 'use strict';
 
 (() => {
-  const AVAILABLE_IMAGES = [
-    '1.jpg',
-    '2.jpg',
-    '3.jpg',
-    '4.jpg',
-    '5.jpg',
-  ];
-  const IMAGE_PATH = 'img';
+
+  const createModal = (dialog) => {
+    let index = 0;
+    let slides = [];
+    const title = dialog.querySelector('[data-role="modal-title"]');
+    const closeBtn = dialog.querySelector('[data-role="modal-close"]');
+    const previewImage = dialog.querySelector('[data-role="preview"]');
+    const prevBtn = dialog.querySelector('[data-role="prev-btn"]');
+    const nextBtn = dialog.querySelector('[data-role="next-btn"]');
+
+    const open = () => {
+      index = 0;
+      dialog.showModal();
+    };
+
+    const close = () => dialog.close();
+
+    const renderPreview = () => {
+      if (!slides[index]) {
+        return;
+      }
+
+      const imgUrl = slides[index];
+      const imgTitleParts = imgUrl.split('/');
+      const titleStr = imgTitleParts[imgTitleParts.length - 1];
+
+      requestAnimationFrame(() => {
+        title.innerText = titleStr;
+        previewImage.src = imgUrl;
+      });
+    }
+
+    const setImages = (images) => {
+      slides = images;
+      index = 0;
+
+      renderPreview();
+    };
+
+    const preview = (imgUrl) => {
+      index = slides.indexOf(imgUrl);
+
+      renderPreview();
+    }
+
+    const prevBtnClickHandler = () => {
+      index -= 1;
+      if (index < 0) {
+        index = slides.length - 1;
+      }
+
+      renderPreview();
+    }
+
+    const nextBtnClickHandler = () => {
+      index += 1;
+      if (index > slides.length - 1) {
+        index = 0;
+      }
+
+      renderPreview();
+    }
+
+    closeBtn.addEventListener('click', close);
+    prevBtn.addEventListener('click', prevBtnClickHandler);
+    nextBtn.addEventListener('click', nextBtnClickHandler);
+
+    return {
+      open,
+      close,
+      setImages,
+      preview,
+    };
+  }
+
 
   document.addEventListener('DOMContentLoaded', () => {
-    const generatePreviewBtn = document.querySelector('[data-role="generate-preview-btn"]');
-    const preview = document.querySelector('[data-role="preview"]');
+    const images = Array.from(document.querySelectorAll('[data-role="image"]'));
+    const modal = document.querySelector('[data-role="modal"]');
 
-    generatePreviewBtn.addEventListener('click', () => {
-      const index = Math.floor(Math.random() * 100 % AVAILABLE_IMAGES.length);
+    const dialog = createModal(modal);
+    dialog.setImages(images.map(img => img.src));
 
-      preview.src = `${IMAGE_PATH}/${AVAILABLE_IMAGES[index]}`;
-      preview.alt = AVAILABLE_IMAGES[index];
+    const imageClickHandler = (event) => {
+      dialog.preview(event.target.src);
+      dialog.open();
+    };
+
+    images.forEach(image => {
+      image.addEventListener('click', imageClickHandler);
     });
   })
 })();
