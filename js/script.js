b3e3060e1d6774a887d6f9a61277078b74221aa5
Bryniuk Valia <valyanychyporuk@gmail.com>
2024-03-02 01:06:59 +0200

    - completed

--- js/script.js
+++ js/script.js
@@ -1,30 +1,51 @@
 'use strict';
 
 (() => {
-  const analyzeText = (text) => {
-    if (typeof text !== 'string') {
-      throw new Error('text must be a valid string');
-    }
+  const getCache = () => {
+    const cache = new WeakMap();
 
-    const normalizedText = text.trim().toLowerCase().replaceAll(/[0-9!@#$%^&*\(\)_+=\-;:\/\\.><\[\]\{\},?]/gis, '');
-    const words = normalizedText.split(' ');
-    const wordsRegistry = new Set(words);
+    return (arg, func) => {
+      if (typeof arg !== 'object') {
+        throw new Error('Arg must be an object');
+      }
 
-    return {
-      uniqueWordsCount: wordsRegistry.size,
-      uniqueWords: Array.from(wordsRegistry),
+      if (cache.has(arg)) {
+        return cache.get(arg);
+      }
+
+      const result = func(arg);
+      cache.set(arg, result);
+
+      return result;
     };
   };
 
 
-try {
-console.log(analyzeText(` Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque convallis augue iaculis sagittis iaculis. Donec quis nulla efficitur, mattis magna in, vulputate neque. Nullam semper, sem eu pharetra dignissim, elit risus imperdiet libero, vitae malesuada nisi sem et quam. Donec a magna vel leo egestas mattis in vel est. Quisque laoreet nibh nisi, at molestie ante rhoncus a. Fusce nec aliquet enim. Nam ultrices nec risus vitae ultrices. Duis ultricies sed augue non fringilla. Quisque eu dolor mi.
+  try {
+    const cacheComplexCalculations = getCache();
 
-Cras id tellus mauris. Donec ultricies et felis ut pretium. Ut faucibus aliquet congue. Morbi id odio mi. Etiam facilisis efficitur pellentesque. Vivamus dictum imperdiet velit quis facilisis. Duis enim mi, sollicitudin lacinia sem a, vestibulum fringilla nibh.
+    const user1 = {user: 1};
+    const user2 = {user: 2};
 
-Nam libero dui, pellentesque nec feugiat sed, varius ac massa. Pellentesque quis tempus lacus. Quisque quis velit ac felis eleifend convallis non vel turpis. Etiam luctus purus ultricies sapien euismod egestas. Morbi tempor risus eget nibh egestas vehicula. Pellentesque viverra metus magna, vel volutpat est mollis eget. Proin et facilisis nisl. Fusce porttitor congue felis sed elementum. Curabitur in erat ac tortor tempus lacinia. Ut et augue non lorem hendrerit egestas. Nulla facilisi. Phasellus ornare faucibus ex, sed rhoncus nisi pharetra et. Maecenas ac tincidunt sapien, vel tempor quam. `));
+    console.log(cacheComplexCalculations(user1, (arg) => {
+      console.log('run first function');
 
-} catch (e) {
-  console.error(e);
-}
+      return arg.user;
+    }))
+
+    console.log(cacheComplexCalculations(user2, (arg) => {
+      console.log('run second function');
+
+      return arg.user;
+    }))
+
+    console.log(cacheComplexCalculations(user1, (arg) => {
+      console.log('run third function');
+
+      return arg.user;
+    }))
+
+  } catch (e) {
+    console.error(e);
+  }
 })();
